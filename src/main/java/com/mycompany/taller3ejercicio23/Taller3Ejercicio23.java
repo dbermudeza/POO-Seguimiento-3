289a08b16a73e9dfa70ec44757d47aa2025240c3
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-09-14 13:44:28 -0500

    Interfaz grafica que retorna las posibles soluciones de una ecuación de segundo grado dados sus parámetros.

---
+++ src/main/java/com/mycompany/taller3ejercicio23/Taller3Ejercicio23.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller3ejercicio23;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Taller3Ejercicio23 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
