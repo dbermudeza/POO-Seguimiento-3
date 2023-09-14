2802fb57d64d143e4f379d83bf34f020a6eb3da3
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-09-14 11:58:10 -0500

    Interfaz grafica que retorna el valor de la altura, el area y el perimetro de un triangulo equilatero en base al valor de sus lados.

---
+++ src/main/java/com/mycompany/taller3ejercicio19/Taller3Ejercicio19.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller3ejercicio19;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Taller3Ejercicio19 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
