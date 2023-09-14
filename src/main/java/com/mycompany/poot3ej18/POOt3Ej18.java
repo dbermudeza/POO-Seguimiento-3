e7a71220e211fc29bd537c983f05c30c7847c12b
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-09-14 11:40:49 -0500

    Interfaz grafica que retorna el salario bruto y neto de un empleado.

---
+++ src/main/java/com/mycompany/poot3ej18/POOt3Ej18.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poot3ej18;
+
+/**
+ *
+ * @author Usuario
+ */
+public class POOt3Ej18 {
+
+    public static void main(String[] args) {
+      Formulario form = new Formulario();
+      form.setVisible(true);
+    }
+}
