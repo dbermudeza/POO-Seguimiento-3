029ed6c529c4254adbb31da3621bca1a4d3c0928
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-09-14 12:19:11 -0500

    Interfaz grafica que compara dos valores y retorna cual es el mayor, o en caso de darse, si son iguales.

---
+++ src/main/java/com/mycompany/taller3ejercicio7/Taller3Ejercicio7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller3ejercicio7;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Taller3Ejercicio7 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
