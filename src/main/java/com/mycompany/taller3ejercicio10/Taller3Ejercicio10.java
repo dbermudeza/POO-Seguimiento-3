5b3f1caa35011b98ff3d1d79ccca318b75d443b3
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-09-14 12:43:38 -0500

    Interfaz grafica que retorna el valor de la matricula de un estudiante dependiendo de su patrimonio y su estrato.

---
+++ src/main/java/com/mycompany/taller3ejercicio10/Taller3Ejercicio10.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller3ejercicio10;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Taller3Ejercicio10 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
