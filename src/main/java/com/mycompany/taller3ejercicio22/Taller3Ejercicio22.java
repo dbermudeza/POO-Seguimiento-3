86fef280efdf43752c34b8df24b4a27c7bbbf84d
Usuario <Usuario@DESKTOP-7BNGDFF>
2023-09-14 13:26:25 -0500

    Interfaz grafica que retorna el salario mensual de un trabajador si este supera los 450000$.

---
+++ src/main/java/com/mycompany/taller3ejercicio22/Taller3Ejercicio22.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller3ejercicio22;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Taller3Ejercicio22 {
+
+    public static void main(String[] args) {
+       Formulario form = new Formulario();
+       form.setVisible(true);
+    }
+}
